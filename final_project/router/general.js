9f2389105b611b01f5dfccab3db8a219b71ebc9f
whiteSaber0 <whitesaber0@gmail.com>
2026-07-01 19:22:39 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,10 +5,24 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+      return res.status(404).json({ message: "Missing username or password" });
+    } else if (isValid(username)) {
+      return res.status(404).json({ message: "user already exists." });
+    } else {
+      users.push({ username: username, password: password });
+      return res
+        .status(200)
+        .json({ message: "User successfully registered.  Please login." });
+    }
+  });
+  
+
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -19,7 +33,7 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn]));
+    res.send(JSON.stringify(books[isbn].rev));
  });
   
 // Get book details based on author
@@ -61,14 +75,10 @@ public_users.get('/title/:title',function (req, res) {
     return res.status(200).send(JSON.stringify(matchingTitle, null, 4));
 });
 
-// Get book review
-public_users.get('/review/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
   
-    const matchingReviews = (books[isbn] && books[isbn].reviews) ? books[isbn].reviews : [];
-  
-    return res.status(200).send(JSON.stringify(matchingReviews, null, 4));
-  });
+});
 
 module.exports.general = public_users;
 
