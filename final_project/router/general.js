e8a27e147e42019f31ec7b6202aff62baa823216
whiteSaber0 <whitesaber0@gmail.com>
2026-07-01 19:53:16 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,59 +36,95 @@ public_users.get('/', function (req, res) {
   });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+const axios = require("axios");
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', async function (req, res) {
+  try {
     const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn].rev));
- });
+
+    // Example: replace this URL with the real endpoint your course/task expects
+    // (e.g., the API that holds book details)
+    const response = await axios.get(`http://localhost:3000/books/isbn/${isbn}`);
+
+    // If the book details are in response.data:
+    return res.status(200).json(response.data);
+  } catch (err) {
+    return res.status(500).json({
+      error: err.response?.data?.message || err.message || "Internal Server Error",
+    });
+  }
+});
   
+
 // Get book details based on author
-// Get book details based on author
+// Get book details based on Author (Promise callbacks)
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
-
-    // Obtain all keys for the 'books' object
-    const keys = Object.keys(books);
-
-    // Iterate and collect matching books
-    let matchingBooks = [];
-    for (let i = 0; i < keys.length; i++) {
-        const key = keys[i];
-        if (books[key].author === author) {
+  
+    Promise.resolve()
+      .then(() => {
+        // Obtain all keys for the 'books' object
+        const keys = Object.keys(books);
+  
+        // Iterate and collect matching books
+        let matchingBooks = [];
+        for (let i = 0; i < keys.length; i++) {
+          const key = keys[i];
+          if (books[key].author === author) {
             matchingBooks.push(books[key]);
+          }
         }
-    }
-
-    return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
-});
+  
+        return matchingBooks;
+      })
+      .then((matchingBooks) => {
+        return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+      })
+      .catch((err) => {
+        return res.status(500).send({ error: err.message || "Internal Server Error" });
+      });
+  });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+// Get all books based on title using Promise with callback
+public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
-
-    // Obtain all keys for the 'books' object
-    const keys = Object.keys(books);
-
-    // Iterate and collect matching books
-    let matchingTitle = [];
-    for (let i = 0; i < keys.length; i++) {
-        const key = keys[i];
-        if (books[key].title === title) {
-            matchingTitle.push(books[key]);
+  
+    const getBooksByTitle = () => {
+      return new Promise((resolve, reject) => {
+        try {
+          const keys = Object.keys(books);
+          const matchingTitle = [];
+  
+          for (let i = 0; i < keys.length; i++) {
+            const key = keys[i];
+            if (books[key].title === title) {
+              matchingTitle.push(books[key]);
+            }
+          }
+  
+          resolve(matchingTitle);
+        } catch (err) {
+          reject(err);
         }
-    }
+      });
+    };
+  
+    getBooksByTitle()
+      .then((matchingTitle) => {
+        return res.status(200).send(JSON.stringify(matchingTitle, null, 4));
+      })
+      .catch(() => {
+        return res.status(500).send('Server error');
+      });
+  });
 
-    return res.status(200).send(JSON.stringify(matchingTitle, null, 4));
-});
 //  Get book review
-public_users.get("/review/:isbn", function (req, res) {
-  const ISBN = req.params.isbn;
-  const Book = books[ISBN];
-  if (Book) {
-    return res.status(200).send(JSON.stringify(Book.reviews, null, 4));
-  } else {
-    return res.status(404).json({ message: "ISBN not found." });
-  }
+public_users.get('/review/:isbn',function (req, res) {
+  
 });
 
 module.exports.general = public_users;
+
 
